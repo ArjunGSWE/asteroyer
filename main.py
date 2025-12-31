d54606ab5e04523ea561bc4d08010549b2d12cde
Arjun <arjungswe@gmail.com>
2025-12-31 10:38:04 +0530

    player movements

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import PLAYER_RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+import player
 
 
 def main():
-    print("Starting Asteroids with pygame version: VERSION")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_1 = player.Player(
+        x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2, radius=PLAYER_RADIUS
+    )
+    dt = 0
 
+    while True:
+        log_state()
 
-while True:
-    log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+        screen.fill(color="black")
 
-    screen.fill(color="black")
-    pygame.display.flip()
+        player_1.draw(screen)
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+        dt = clock.tick(60) / 1000
+
+        player_1.update(dt)
+
 
 if __name__ == "__main__":
     main()
